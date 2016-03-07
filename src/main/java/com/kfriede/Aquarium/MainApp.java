d05d64fd213f58a61434418cf283c8ede1b703c6
Kevin Friedemann <kevin.friedemann@gmail.com>
2016-03-06 17:14:19 -0800

    Added icon

--- src/main/java/com/kfriede/Aquarium/MainApp.java
+++ src/main/java/com/kfriede/Aquarium/MainApp.java
@@ -2,26 +2,20 @@ package com.kfriede.Aquarium;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.util.Properties;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApp extends Application{
 	private static final String MAIN_VIEW_FILE_PATH = "MainWindow.fxml";
 	private static final String APPLICATION_TITLE = "Aquarium ";
 	private static final String PROPERTIES_FILE = "project.properties";
+	private static final String ICON_FILE = "icon.png";
 	
 	public static Properties PROPERTIES = new Properties();;
 
@@ -32,6 +26,8 @@ public class MainApp extends Application{
 		Parent root = FXMLLoader.load(getClass().getResource(MAIN_VIEW_FILE_PATH));
 
 		Scene scene = new Scene(root);
+		
+		stage.getIcons().add(new Image(ICON_FILE));
 
 		stage.setTitle(APPLICATION_TITLE + PROPERTIES.getProperty("version"));
 		stage.setScene(scene);
